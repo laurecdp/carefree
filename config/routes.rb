169c5f61561d7161824a2ad51e374f9d8f8be1a8
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-02-22 16:22:59 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :actions, only: [:new, :create, :edit, :update]
+  resources :patients, only: [:new, :create, :index, :edit, :update, :show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
