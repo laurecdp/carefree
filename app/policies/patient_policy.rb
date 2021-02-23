308295ff004a61c07837b1b3c17e76a646734c26
Sami Ziani <zianisami0@gmail.com>
2021-02-23 11:13:51 +0100

    init pundit

---
+++ app/policies/patient_policy.rb
@@ -0,0 +1,15 @@
+class PatientPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+  end
+end
