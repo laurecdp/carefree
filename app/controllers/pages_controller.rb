26394878ef8eaf3064390bd1ecb5e52861e0d0f2
Sami Ziani <zianisami0@gmail.com>
2021-02-23 15:31:05 +0100

    design login page margin top

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :dashboard ]
 
   def home
-    if user_signed_in?
-      redirect_to dashboard_path
-    end
   end
 
   def dashboard
+  end
+
+  def login
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
+  get '/login', to: 'pages#login'
   resources :actions, only: [:new, :create, :edit, :update]
   resources :patients, only: [:new, :create, :index, :edit, :update, :show]
 
