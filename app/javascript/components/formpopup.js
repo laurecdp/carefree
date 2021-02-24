c05d596668127085ef5206b619a95a2e427ca7a9
Sami Ziani <zianisami0@gmail.com>
2021-02-24 18:11:54 +0100

    form naissance

--- app/javascript/components/formpopup.js
+++ app/javascript/components/formpopup.js
@@ -1,10 +1,13 @@
 const popup = () => {
   const naissance = document.getElementById("baby-sidebar");
-  naissance.addEventListener("click", (event) => {
-    // $("#modal").show();
-    $('#modal').modal('show');
-    event.preventDefault();
-  });
+  const naissance2 = document.getElementById("baby-card");
+  [naissance, naissance2].forEach((card) => {
+    card.addEventListener("click", (event) => {
+      // $("#modal").show();
+      $('#modal').modal('show');
+      event.preventDefault();
+    });
+  })
 };
 
 export { popup }
