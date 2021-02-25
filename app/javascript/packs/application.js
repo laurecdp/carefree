e915b9b7650cc444bec55fc0d460bcc4b75eb587
Manon Garnier <manon.garnier.lehy@gmail.com>
2021-02-25 15:51:17 +0100

    added searchwindow on dashboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,6 +28,7 @@ import "bootstrap";
 // Internal imports, e.g:
 import { sidebar } from '../components/sidebar';
 import { popup } from '../components/formpopup';
+import { searchWindow } from '../components/search-window';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
