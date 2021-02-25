dd598cf55e0b2487c72d92651f06aba97a24e0d1
Manon Garnier <manon.garnier.lehy@gmail.com>
2021-02-25 20:23:28 +0100

    changed colors in dashboard cards

--- app/javascript/components/search-window.js
+++ app/javascript/components/search-window.js
@@ -1,12 +1,16 @@
 const searchWindow = () => {
-  const naissance = document.querySelector(".sidebar .action4");
+  const naissanceBtn1 = document.querySelector(".sidebar .action4");
+  const naissanceBtn2 = document.querySelector(".button-action3");
   const searchButton = document.querySelector(".search-form-control .btn");
   const searchWindow = document.querySelector(".search-window");
   if (searchWindow) {
-    naissance.addEventListener("click", (event) => {
+    naissanceBtn1.addEventListener("click", (event) => {
       searchWindow.classList.toggle("appear");
     });
     searchButton.addEventListener("click", (event) => {
+      searchWindow.classList.toggle("appear");
+    });
+    naissanceBtn2.addEventListener("click", (event) => {
       searchWindow.classList.toggle("appear");
     });
   }
