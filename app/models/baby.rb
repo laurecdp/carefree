b60426e3915cd69b49035935166a3c2f77297e64
Sami Ziani <zianisami0@gmail.com>
2021-02-26 12:39:13 +0100

    implement baby creation

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -1,24 +1,42 @@
 class Baby < ApplicationRecord
-  belongs_to :patient
-
   SEX = ['masculin', 'féminin', 'indéterminé']
   validates :sex, inclusion: { in: SEX }
 
   MONITORINGOPTIONS = ['Fréquence cardiaque', 'Mise en incubateur', 'Saturation', 'Glycémie']
-  validates :monitoring_options, inclusion: { in: MONITORINGOPTIONS }
+  validate :each_monitoring_options
 
   INTENSIVECAREOPTIONS = ['Ventilation au masque', 'Massage cardiaque', 'Incubation', 'Injection médicamenteuse']
-  validates :intensivecare_options, inclusion: { in: INTENSIVECAREOPTIONS }
+  validate :each_intensive_care_options
 
   INFECTIOUSCONTEXTOPTIONS = ['antibiotiques à la naissance', 'prélèvements bactériologiques','liquide amniotique teinté','fièvre maternelle > 38°', 'infection maternelle']
-  validates :infectiouscontext_options, inclusion: { in: INFECTIOUSCONTEXTOPTIONS }
+  validate :each_infectious_context_options
 
   EXITROOM = ['Avec sa mère', 'Décès', 'Transfert immédiat', 'Mutation intra-muros']
   validates :exit_room, inclusion: { in: EXITROOM }
 
   PATHOLOGIES = ['Retard de croissance', 'SIDA', 'Diabète']
-  validates :pathologies, inclusion: { in: PATHOLOGIES }
+  validate :each_pathologies
 
   BREASTFEEDING = ['Maternel', 'Artificiel', 'Mixte']
   validates :breastfeeding, inclusion: { in: BREASTFEEDING }
+
+  belongs_to :patient
+
+  private
+
+  def each_monitoring_options
+    monitoring_options.map { |baby| MONITORINGOPTIONS.include?(baby) }.all?
+  end
+
+  def each_intensive_care_options
+    intensivecare_options.map { |baby| INTENSIVECAREOPTIONS.include?(baby) }.all?
+  end
+
+  def each_infectious_context_options
+    infectiouscontext_options.map { |baby| INFECTIOUSCONTEXTOPTIONS.include?(baby) }.all?
+  end
+
+  def each_pathologies
+    pathologies.map { |baby| PATHOLOGIES.include?(baby) }.all?
+  end
 end
