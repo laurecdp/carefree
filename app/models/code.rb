68e3e78809dad73e9cb8fe7885fb1e66d0aca6d6
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-02-27 14:19:10 +0100

    change on model codes

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,6 +1,6 @@
 class Code < ApplicationRecord
   has_many :labour_codes
 
-  CATEGORIES = ['césarienne', 'voie naturelle', 'Actes', 'Complication']
+  CATEGORIES = ['Césarienne', 'Voie naturelle', 'Actes', 'Complication']
   validates :category, inclusion: { in: CATEGORIES }
 end
