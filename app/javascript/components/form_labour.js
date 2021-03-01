87220d2bee0bf74e3b84848b4a2ea7262787a463
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-03-01 15:21:08 +0100

    form until part 5 done

--- app/javascript/components/form_labour.js
+++ app/javascript/components/form_labour.js
@@ -1,6 +1,6 @@
 const searchForm = () => {
  
-  /*Partie 1 du Form "Résumé de l'accouchement */
+  /* Partie 1 du Form "Résumé de l'accouchement */
   if (document.getElementById("new_labour")) {
 
     const tagItem = document.querySelectorAll(".tag-item");
@@ -32,23 +32,86 @@ const searchForm = () => {
        });
      });
     
-    /*Partie 2 du Form "Anésthésie*/
-    const inputYesRadioButtons = document.getElementById("labour_anaesthesia_oui");
+    /* Partie 2 du Form "Anésthésie */
+    /* If yes */
+    const inputYesAnesthesia = document.getElementById("labour_anaesthesia_oui");
 
-    inputYesRadioButtons.addEventListener("click", (event) => {
-      document.getElementById("moment-anesthesia").style.display = "block"
+    inputYesAnesthesia.addEventListener("click", (event) => {
+      document.getElementById("moment-anesthesia").style.display = "block" 
       document.getElementById("category-anesthesia").style.display = "block"
       document.getElementById("anesthia-checkbox").style.display = "block"
       
-    })
-    
-    const inputNoRadioButtons = document.getElementById("labour_anaesthesia_non");
+      let categoryAnesthesia = document.getElementById("category-anesthesia")
+      categoryAnesthesia.style.display = "block"
 
-    inputNoRadioButtons.addEventListener("click", (event) => {
+        const tagItem = document.querySelectorAll(".tag-item");
+        tagItem.forEach(element => {
+          element.addEventListener("click", (event) => {
+            if (element.querySelector("input").id === "labour_anaesthesia_category_anesthésie_loco-régionale") {
+              document.getElementById("anesthesia-loco-general").style.display = "block"
+              document.getElementById("anesthesia-general").style.display = "none"
+            } else if (element.querySelector("input").id === "labour_anaesthesia_category_anesthésie_générale") {
+              document.getElementById("anesthesia-general").style.display = "block"
+              document.getElementById("anesthesia-loco-general").style.display = "none"
+            } else if (element.querySelector("input").id === "labour_anaesthesia_category_anesthésie_locale") {
+              document.getElementById("anesthesia-general").style.display = "block"
+              document.getElementById("anesthesia-loco-general").style.display = "none"
+            }; 
+          })
+        })
+      
+    })
+
+    /* If no */
+    const inputNoAnesthesia = document.getElementById("labour_anaesthesia_non");
+
+    inputNoAnesthesia.addEventListener("click", (event) => {
       document.getElementById("moment-anesthesia").style.display = "none"
       document.getElementById("category-anesthesia").style.display = "none"
       document.getElementById("anesthia-checkbox").style.display = "none"
+      document.getElementById("anesthesia-loco-general").style.display = "none"
+      document.getElementById("anesthesia-general").style.display = "none"
     })
+
+  /* Partie 4 du Form "Complications" */
+
+    /* If yes */
+    const inputYesComplication = document.getElementById("labour_labour_complication_oui");
+
+    inputYesComplication.addEventListener("click", (event) => {
+      document.getElementById("complication-type").style.display = "block"
+      document.getElementById("complication-checkbox").style.display = "block"
+    })
+
+    /* If no */
+    const inputNoComplication = document.getElementById("labour_labour_complication_non");
+
+    inputNoComplication.addEventListener("click", (event) => {
+      document.getElementById("complication-checkbox").style.display = "none"
+      document.getElementById("complication-type").style.display = "none"
+    })
+
+  /* Partie 5 du Form "Actes" */
+
+    /* If yes */
+    const inputYesActes = document.getElementById("labour_labour_actes_done_oui");
+
+    inputYesActes.addEventListener("click", (event) => {
+      document.getElementById("labour-actes").style.display = "block"
+      document.getElementById("delivrance-checkbox").style.display = "block"
+    })
+
+    /* If no */
+    const inputNoActes = document.getElementById("labour_labour_actes_done_non");
+
+    inputNoActes.addEventListener("click", (event) => {
+      document.getElementById("delivrance-checkbox").style.display = "none"
+      document.getElementById("labour-actes").style.display = "none"
+    })
+  
+  /* Partie 6 du Form "Informations du bébé" */
+
+  
 
   }
 }
