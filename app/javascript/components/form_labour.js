a6ae0a4f779b00b6fe910da6f8a1aca1c0719503
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-03-01 11:32:18 +0100

    add category pharmacie

--- app/javascript/components/form_labour.js
+++ app/javascript/components/form_labour.js
@@ -4,19 +4,26 @@ const searchForm = () => {
     const tagItem = document.querySelectorAll(".tag-item");
      tagItem.forEach(element => {
        element.addEventListener("click", (event) => {
+
          if (element.querySelector("input").id === "labour_labour_start_travail_spontané") {
-          console.log("travail spontané")
+          document.getElementById("travail-spontane-checkbox").style.display = "block"
+           document.getElementById("cesarienne-checkbox").style.display = "none"
+           document.getElementById("artificial-labour").style.display = "none"
+          
          } else if (element.querySelector("input").id === "labour_labour_start_déclenchement_artificiel") {
+           document.getElementById("travail-spontane-checkbox").style.display = "block"
+           document.getElementById("cesarienne-checkbox").style.display = "none"
+           document.getElementById("artificial-labour").style.display = "block"
 
          } else if (element.querySelector("input").id === "labour_labour_start_césarienne_avant_travail") {
-           
+           document.getElementById("cesarienne-checkbox").style.display = "block"
+           document.getElementById("travail-spontane-checkbox").style.display = "none"
+           document.getElementById("artificial-labour").style.display = "block"
          }
     
        });
      });
   }
-
-
  
 }
 
