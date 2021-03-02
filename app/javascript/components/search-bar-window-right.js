ad36569546fcbfaf2c769460b516342264929489
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-03-02 10:05:31 +0100

    show html + javascript

--- app/javascript/components/search-bar-window-right.js
+++ app/javascript/components/search-bar-window-right.js
@@ -2,21 +2,24 @@ import 'js-autocomplete/auto-complete.css';
 import autocomplete from 'js-autocomplete';
 
 const searchbirthautocomplete = function() {
-  const patientsright = JSON.parse(document.querySelector('.search-right').dataset.patients)
-  const searchInputright = document.getElementById('search_right_query')
-  if (patientsright && searchInputright) {
-    new autocomplete({
-      selector: searchInputright,
-      minChars: 1,
-      source: function(term, suggest){
+  const searchRight = document.querySelector('.search-right');
+  if (searchRight) {
+    const patientsright = JSON.parse(searchRight.dataset.patients)
+    const searchInputright = document.getElementById('search_right_query')
+    if (patientsright && searchInputright) {
+      new autocomplete({
+        selector: searchInputright,
+        minChars: 1,
+        source: function (term, suggest) {
           term = term.toLowerCase();
           const choices = patientsright;
           const matches = [];
           for (let i = 0; i < choices.length; i++)
-              if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
+            if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
           suggest(matches);
-      },
-    });
+        },
+      });
+  }
   }
 };
 
