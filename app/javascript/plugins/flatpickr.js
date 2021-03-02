76bc5e6da3c0d5f60262d6d96d72c1c74ae531a7
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-03-02 22:37:55 +0100

    form with datepicker flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,26 @@
 import flatpickr from "flatpickr";
+import { French } from "flatpickr/dist/l10n/fr.js"
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {inline: true});
+  flatpickr(".datepicker", {
+  inline: true,
+  defaultDate: "today",
+  dateFormat: "d-m-Y H:i",
+  "locale": French,
+  });
+
+  flatpickr(".datetime-picker", {
+    altInput: true,
+    altFormat: "d F Y  -  H:i",
+    ariaDateFormat: "d F Y  -  H:i",
+    enableTime: true,
+    allowInput: true,
+    defaultDate: "today",
+    dateFormat: "d-m-Y H:i",
+    minuteIncrement: 5,
+    "locale": French,
+    maxDate: new Date().fp_incr(1) // 1 day from now
+  });
 }
 
 export { initFlatpickr };
